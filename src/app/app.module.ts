a05d69ffad4b87342e6087f0a2202c6560e493bd
ismailOfficial112 <135072970+ismailOfficial112@users.noreply.github.com>
2023-08-04 18:49:46 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { I18nModule } from './i18n/i18n.module';
 import { SelectLanguageComponent } from './select-language/select-language.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectLanguageComponent
+    SelectLanguageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
      I18nModule,
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
