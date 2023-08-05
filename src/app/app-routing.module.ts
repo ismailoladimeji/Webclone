80bda9b2f5bc623adc1358730c75442b97004750
ismailOfficial112 <135072970+ismailOfficial112@users.noreply.github.com>
2023-08-05 18:00:15 +0100

    object

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  // { 
+  //   path: 'expenses', 
+  //   component: ExpenseEntry4Component
+  // },
+  // { 
+  //   path: 'expenses/detail/:id', 
+  //   component: ExpenseEntryComponent 
+  // }, 
+  // { 
+  //   path: '', redirectTo: 'expenses', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+
+ }
